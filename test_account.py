f718176cb90eb9d1bd187206ece7cd9cce77d6b0
Ruben Colomina <rcolomina@gmail.com>
2022-05-02 13:08:34 +0100

    add stub test

--- test_account.py
+++ test_account.py
@@ -3,7 +3,9 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+        pass
+        #self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == '__main__':
